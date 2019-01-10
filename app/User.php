d3557cfcd7e47d943a7920979f0dc2c095c21ac8
fakorede <abiolafakorede@gmail.com>
2019-01-10 04:14:22 +0100

    setup for api including jwt

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -9,21 +10,21 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function getJWTIdentifier() {
+        // return the primary key of the user
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims() {
+        // return a key-value array containing any custom claims to be added to the JWT
+        return [];
+    }
 }
