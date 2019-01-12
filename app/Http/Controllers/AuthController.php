65ec1b2ad7176f5b790342aed2c937de3a4d5069
fakorede <abiolafakorede@gmail.com>
2019-01-12 04:24:53 +0100

    created login, logout and user methods

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,20 +3,54 @@
 namespace App\Http\Controllers;
 
 use App\User;
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use App\Http\Requests\UserRegisterRequest;
+use App\Http\Requests\UserLoginRequest;
 use App\Http\Resources\User as UserResource;
 
 class AuthController extends Controller
 {
     public function register(UserRegisterRequest $request) {
 
-        $user  = User::create([
+        $user = User::create([
             'email' => $request->email,
             'name' => $request->name,
             'password' => bcrypt($request->passsword)
         ]);
 
-        return new UserResource($user);
+        if(!$token = auth()->attempt($request->only(['email', 'password']))) {
+            return abort(401);
+        }
+        
+        return (new UserResource($request->user()))->additional([
+            'meta' => [
+                'token' => $token
+            ]
+        ]);
     }
+
+    public function login(UserLoginRequest $request) {
+        if(!$token = auth()->attempt($request->only(['email', 'password']))) {
+            return response()->json([
+                'errors' => [
+                    'email' => ['Email address incorrect!']
+                ]
+            ], 422);
+        }
+        
+        return (new UserResource($request->user()))->additional([
+            'meta' => [
+                'token' => $token
+            ]
+        ]);
+    }
+
+    public function logout(Request $request) {
+        auth()->logout();
+    }
+
+    public function user(Request $request) {
+        return new UserResource($request->user());
+    }
+
 }
