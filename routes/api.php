561a8a6b9401ddc06f9a4cf45c1f3883655c460c
fakorede <abiolafakorede@gmail.com>
2019-01-12 04:23:20 +0100

    created routes for login and logout

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,9 @@
 <?php
 
 Route::post('/register', 'AuthController@register');
+Route::post('/login', 'AuthController@login');
+Route::post('/logout', 'AuthController@logout');
+
+// User endpoint
+Route::get('/user', 'AuthController@user');
 
