2be1da67f9a5e35213550e8c4be195aedab50b4f
fakorede <abiolafakorede@gmail.com>
2019-02-01 21:24:08 +0100

    implemented topic update

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\TopicCreateRequest;
+use App\Http\Requests\UpdateTopicRequest;
 // use Illuminate\Http\Request;
 use App\Post;
 use App\Topic;
@@ -31,6 +32,12 @@ class TopicController extends Controller
     }
 
     public function show(Topic $topic) {
+        return new TopicResource($topic);
+    }
+
+    public function update(UpdateTopicRequest $request, Topic $topic) {
+        $topic->title = $request->get('title', $topic->title);
+        $topic->save();
         return new TopicResource($topic);
     }
 }
