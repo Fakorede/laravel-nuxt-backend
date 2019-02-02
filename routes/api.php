0562bf266910da4b1a3640c492a7cafa16ddda42
fakorede <abiolafakorede@gmail.com>
2019-02-02 11:49:16 +0100

    created route to add new topic to a post

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,10 @@ Route::group(['prefix' => 'topics'], function() {
     Route::get('/{topic}', 'TopicController@show');
     Route::patch('/{topic}', 'TopicController@update')->middleware('auth:api');
     Route::delete('/{topic}', 'TopicController@destroy')->middleware('auth:api');
+
+    // Post routes group
+    Route::group(['prefix' => '/{topic}/posts'], function() {
+        Route::post('/', 'PostController@store')->middleware('auth:api');
+    });
 });
 
